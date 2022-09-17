91ec9fa777cf0120db0a9a8254e61a9d61acc2a3
Sirojiddin Kilichbaev <ssirojiddin2004@gmail.com>
2022-09-17 10:49:06 +0500

    a

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,11 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int one = 1;
+        int ten = 10;
+        int hundred = 100;
+        System.out.println(one);
+        System.out.println(ten);
+        System.out.println(hundred);
     }
 }
